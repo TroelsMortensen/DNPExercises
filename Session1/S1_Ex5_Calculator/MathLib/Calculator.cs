615bf8495bd03f88cc7653b9e478b945cb58fa5a
Troels Mortensen <tromortensen@hotmail.com>
2023-09-01 11:07:10 +0200

    Ex5 done

---
+++ Session1/S1_Ex5_Calculator/MathLib/Calculator.cs
@@ -0,0 +1,20 @@
+﻿namespace S1_Ex5_Calculator.MathLib;
+
+public class Calculator
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static int Add(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
---
+++ Session1/S1_Ex5_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using S1_Ex5_Calculator.MathLib;
+
+Console.WriteLine("Hello, World!");
+
+int result = Calculator.Add(3, 7);
+Console.WriteLine("3 + 7 = " +result);
+
+int[] numbers = new []{3,5,8,9};
+int result1 = Calculator.Add(numbers);
+Console.WriteLine(result1);
