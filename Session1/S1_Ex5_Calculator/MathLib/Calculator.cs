c38f6a18d1a4ffac2a3ce2779c40907eaf2aa5ea
Troels Mortensen <tromortensen@hotmail.com>
2023-09-01 11:13:28 +0200

    Exercise 6 done

--- Session1/S1_Ex5_Calculator/MathLib/Calculator.cs
+++ Session1/S1_Ex5_Calculator/MathLib/Calculator.cs
@@ -17,4 +17,27 @@ public class Calculator
 
         return sum;
     }
+
+
+    // Exercise 6
+    public static void LargestOfTwo()
+    {
+        string s1 = Console.ReadLine();
+        string s2 = Console.ReadLine();
+        int num1 = Int32.Parse(s1);
+        int num2 = Int32.Parse(s2);
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("Largest: " + num1);
+        }
+        else if (num2 > num1)
+        {
+            Console.WriteLine("Largest: " + num2);
+        }
+        else
+        {
+            Console.WriteLine("Numbers are equal");
+        }
+    }
 }
--- Session1/S1_Ex5_Calculator/Program.cs
+++ Session1/S1_Ex5_Calculator/Program.cs
@@ -10,3 +10,7 @@ Console.WriteLine("3 + 7 = " +result);
 int[] numbers = new []{3,5,8,9};
 int result1 = Calculator.Add(numbers);
 Console.WriteLine(result1);
+
+
+// Exercise 6
+Calculator.LargestOfTwo();
