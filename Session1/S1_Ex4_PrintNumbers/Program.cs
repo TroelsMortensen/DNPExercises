c3bf4c27ae1002d072a889b346e7ebfec43c40b3
Troels Mortensen <tromortensen@hotmail.com>
2023-09-01 10:57:05 +0200

    ex4 c)

---
+++ Session1/S1_Ex4_PrintNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+PrintEventNumbers(10);
+Console.WriteLine("--------------------");
+PrintEventNumbers(30);
+
+void PrintEventNumbers(int x)
+{
+    for (int i = 0; i < x; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(x);
+        }
+    }    
+}
