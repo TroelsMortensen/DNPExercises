9fdaefa4fb8842a5ee2921d6f6b0013836844c69
Troels Mortensen <tromortensen@hotmail.com>
2023-09-01 11:22:25 +0200

    Ex9 done

---
+++ Session1/S1_Ex9_Substring/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+MakeSubString("Hello", 2);
+MakeSubString("Chocolate", 3);
+MakeSubString("Chocolate", 1);
+
+void MakeSubString(string input, int i)
+{
+    string start = input.Substring(0, i);
+    string end = input.Substring(input.Length - i, i);
+    Console.WriteLine(start + end);
+}
