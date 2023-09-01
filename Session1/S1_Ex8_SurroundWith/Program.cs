e552b2fa43659f3861c02a31c40896c93e714b28
Troels Mortensen <tromortensen@hotmail.com>
2023-09-01 11:19:20 +0200

    Ex8 done

---
+++ Session1/S1_Ex8_SurroundWith/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+MakeOuterWord("<<>>", "Yay");
+MakeOuterWord("<<>>", "WooHoo");
+MakeOuterWord("[[]]", "word");
+
+void MakeOuterWord(string outer, string inner)
+{
+    string outerStart = outer.Substring(0, 2);
+    string outerEnd = outer.Substring(2, 2);
+    string result = outerStart + inner + outerEnd;
+    Console.WriteLine(result);
+}
