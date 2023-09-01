2213b9c303ede304066c6df3c2537fb4a15a8ec2
Troels Mortensen <tromortensen@hotmail.com>
2023-09-01 11:25:46 +0200

    Ex10 done

---
+++ Session1/S1_Ex10_BiggestDiff/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+FindDiff(new []{10, 3, 5, 6});
+FindDiff(new []{7, 2, 10, 9});
+FindDiff(new []{2, 10, 7, 2});
+
+void FindDiff(int[] numbers)
+{
+    int smallest = numbers[0];
+    int largest = numbers[0];
+    foreach (int number in numbers)
+    {
+        if (number < smallest) smallest = number;
+        if (number > largest) largest = number;
+    }
+
+    Console.WriteLine(largest-smallest);
+}
