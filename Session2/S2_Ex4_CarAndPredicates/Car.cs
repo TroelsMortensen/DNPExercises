88111ae556523b653bcc35b69a720e514fb4155f
Troels Mortensen <tromortensen@hotmail.com>
2023-09-05 11:29:22 +0200

    setup done

--- Session2/S2_Ex4_CarAndPredicates/Car.cs
+++ Session2/S2_Ex4_CarAndPredicates/Car.cs
@@ -1,6 +1,18 @@
-﻿namespace S2_Ex4_CarAndPredicates;
+﻿using System.Text.Json;
+
+namespace S2_Ex4_CarAndPredicates;
 
 public class Car
 {
-    
+    public string Color { get; set; }
+    public int HorsePowers { get; set; }
+    public int FuelEconomy { get; set; }
+    public bool IsManualShift { get; set; }
+    public int NumberOfDoors { get; set; }
+
+    public override string ToString()
+    {
+        // this is a sneaky way to easily create a string representation of this object.
+        return JsonSerializer.Serialize(this);
+    }
 }
--- Session2/S2_Ex4_CarAndPredicates/Program.cs
+++ Session2/S2_Ex4_CarAndPredicates/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using S2_Ex4_CarAndPredicates;
+
 Console.WriteLine("Hello, World!");
+List<Car> cars = GenerateCars();
+
+
+
+// methods:
+
+List<Car> GenerateCars()
+{
+    List<Car> cars = new();
+    string[] colors = new[] { "red", "blue", "green", "yellow", "black" };
+    Random random = new Random(0); // hardcoded seed should give the same cars each time we run the program
+    for (int i = 0; i < 10; i++)
+    {
+        string color = colors[random.Next(colors.Length)];
+        int horsepowers = random.Next(80, 400);
+        int fuelEconomy = random.Next(10, 30);
+        bool isManual = random.Next(1) == 0;
+        int doors = random.Next(3, 6);
+        cars.Add(
+            new Car
+            {
+                FuelEconomy = fuelEconomy,
+                IsManualShift = isManual,
+                Color = color,
+                HorsePowers = horsepowers,
+                NumberOfDoors = doors
+            }
+        );
+    }
+    
+    return cars;
+}
+
