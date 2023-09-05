3dfe407e8c8d293a17644e7bc3676d15355eb88b
Troels Mortensen <tromortensen@hotmail.com>
2023-09-05 10:33:47 +0200

    company exercise done

--- Session2/S2_Ex1_Polymorphism/Program.cs
+++ Session2/S2_Ex1_Polymorphism/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using S2_Ex1_Polymorphism.CompanyAdminSys;
+
+Console.WriteLine("Starting example...");
+
+Company comp = new Company();
+Employee emp1 = new FullTimeEmployee(1234, "Peter");
+Employee emp2 = new FullTimeEmployee(4325, "Jens");
+Employee emp3 = new PartTimeEmployee(100, 125, "Anna");
+Employee emp4 = new PartTimeEmployee(45, 110, "Julius");
+comp.HireNewEmployee(emp1);
+comp.HireNewEmployee(emp2);
+comp.HireNewEmployee(emp3);
+comp.HireNewEmployee(emp4);
+Console.WriteLine("Total expenses: "+ comp.GetMonthlySalaryTotal());
