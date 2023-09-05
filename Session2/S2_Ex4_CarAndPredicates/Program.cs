3b15136ceb62129aefd4750374ab6b77407476f2
Troels Mortensen <tromortensen@hotmail.com>
2023-09-05 11:33:18 +0200

    find by color done

--- Session2/S2_Ex4_CarAndPredicates/Program.cs
+++ Session2/S2_Ex4_CarAndPredicates/Program.cs
@@ -5,23 +5,35 @@ using S2_Ex4_CarAndPredicates;
 Console.WriteLine("Hello, World!");
 List<Car> cars = GenerateCars();
 
+Console.WriteLine("--- Finding blue cars ---");
+List<Car> blueCars = FindByColor(cars, "blue");
+PrintListOfCars(blueCars);
+
+
+void PrintListOfCars(List<Car> list)
+{
+    foreach (Car car in list)
+    {
+        Console.WriteLine(car);
+    }
+}
 
 
 // methods:
 
 List<Car> GenerateCars()
 {
-    List<Car> cars = new();
+    List<Car> generatedCars = new();
     string[] colors = new[] { "red", "blue", "green", "yellow", "black" };
     Random random = new Random(0); // hardcoded seed should give the same cars each time we run the program
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 20; i++)
     {
         string color = colors[random.Next(colors.Length)];
         int horsepowers = random.Next(80, 400);
         int fuelEconomy = random.Next(10, 30);
         bool isManual = random.Next(1) == 0;
         int doors = random.Next(3, 6);
-        cars.Add(
+        generatedCars.Add(
             new Car
             {
                 FuelEconomy = fuelEconomy,
@@ -33,6 +45,12 @@ List<Car> GenerateCars()
         );
     }
     
-    return cars;
+    return generatedCars;
 }
 
+List<Car> FindByColor(List<Car> cars, string color)
+{
+    IEnumerable<Car> result = cars.Where(car => car.Color.Equals(color));
+    List<Car> list = result.ToList();
+    return list;
+}
