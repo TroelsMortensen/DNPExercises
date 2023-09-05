589c7d699646c9c195f0d80d7a9ef03b843826e2
Troels Mortensen <tromortensen@hotmail.com>
2023-09-05 11:07:30 +0200

    initial setup with classes and empty methods

---
+++ Session2/S2_Ex3_DoctorsWaitingRoom/Patient.cs
@@ -0,0 +1,16 @@
+﻿namespace S2_Ex3_DoctorsWaitingRoom;
+
+public class Patient
+{
+    private int numberInQueue;
+
+    public Patient(WaitingRoom wr)
+    {
+        
+    }
+
+    private void ReactToNumber(int ticketNumber)
+    {
+        
+    }
+}
---
+++ Session2/S2_Ex3_DoctorsWaitingRoom/WaitingRoom.cs
@@ -0,0 +1,19 @@
+﻿namespace S2_Ex3_DoctorsWaitingRoom;
+
+public class WaitingRoom
+{
+    public Action<int> NumberChange { get; set; }
+
+    private int currentNumber;
+    private int ticketCount;
+
+    public void RunWaitingRoom()
+    {
+    }
+
+    public int DrawNumber()
+    {
+        ticketCount++;
+        return ticketCount;
+    }
+}
