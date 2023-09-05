0e4a0bad9199ff1d6ec12f7287063c4f5364f051
Troels Mortensen <tromortensen@hotmail.com>
2023-09-05 10:51:00 +0200

    taxi class added

--- Session2/S2_Ex2_TrafficLight/Program.cs
+++ Session2/S2_Ex2_TrafficLight/Program.cs
@@ -6,4 +6,6 @@ TrafficLight tl = new();
 new Car(tl, 1);
 new Car(tl, 2);
 new Car(tl, 3);
+new Taxi(tl, 1);
+new Taxi(tl, 2);
 tl.RunTrafficLight();
---
+++ Session2/S2_Ex2_TrafficLight/Taxi.cs
@@ -0,0 +1,31 @@
+﻿namespace S2_Ex2_TrafficLight;
+
+public class Taxi
+{
+    private int id;
+
+    public Taxi(TrafficLight tl, int id)
+    {
+        this.id = id;
+        tl.OnLightChange += ReactToLight;
+    }
+
+    private void ReactToLight(string color)
+    {
+        string result = $"Car {id} ";
+        switch (color)
+        {
+            case "GREEN":
+                result += " races across";
+                break;
+            case "YELLOW":
+                result += " speeds up, crosses while yelling TAXI GREEN";
+                break;
+            case "RED":
+                result += " slams the breaks and comes to a screeching halt";
+                break;
+        }
+
+        Console.WriteLine(result);
+    }
+}
